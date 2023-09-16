675527b591255d93fbae59ac83a05e2498215029
quantanhoi <thieuquangtrung1999@gmail.com>
2023-09-16 23:53:41 +0200

    update

--- Test/awsServer.py
+++ Test/awsServer.py
@@ -7,26 +7,38 @@ CORS(app)
 
 
 class MqttMessage:
-    def __init__(self, sender, recipient, message):
+    def __init__(self, sender, recipient, message, data):
         self.sender = sender
         self.recipient = recipient
         self.message = message
+        self.data = data
     
     def to_json(self):
         return json.dumps(self.__dict__)
 
+#mqtt section
 def get_skill():
-    msg = MqttMessage("awsServer", "homeServer", "getSkill")
+    msg = MqttMessage("awsServer", "homeServer", "getSkill", None)
     mqtt_client.publish("Trung Thieu Quang Portfolio test", msg.to_json())
 
 def get_project():
-    msg = MqttMessage("awsServer", "homeServer", "getProject")
+    msg = MqttMessage("awsServer", "homeServer", "getProject", None)
     mqtt_client.publish("Trung Thieu Quang Portfolio test", msg.to_json())
 
 def get_contacts():
-    msg = MqttMessage("awsServer", "homeServer", "getContacts")
+    msg = MqttMessage("awsServer", "homeServer", "getContacts", None)
     mqtt_client.publish("Trung Thieu Quang Portfolio test", msg.to_json())
 
+def process_message(message):
+    # Your function logic goes here
+    pass
+
+def on_message(client, userdata, msg):
+    print("Received message:", msg.payload.decode())
+    process_message(msg.payload.decode())
+
+
+#https requests handling
 @app.route('/skill')
 def skill_endpoint():
     skills = get_skill()
@@ -44,6 +56,28 @@ def contact_endpoint():
     contacts = get_contacts()
     print(contacts)
     return jsonify(contacts)
+
+
+#MQTT Section
+def get_skill():
+    msg = MqttMessage("awsServer", "homeServer", "getSkill")
+    mqtt_client.publish("Trung Thieu Quang Portfolio test", msg.to_json())
+
+def get_project():
+    msg = MqttMessage("awsServer", "homeServer", "getProject")
+    mqtt_client.publish("Trung Thieu Quang Portfolio test", msg.to_json())
+
+def get_contacts():
+    msg = MqttMessage("awsServer", "homeServer", "getContacts")
+    mqtt_client.publish("Trung Thieu Quang Portfolio test", msg.to_json())
+
+def process_message(message):
+    # Your function logic goes here
+    pass
+
+def on_message(client, userdata, msg):
+    print("Received message:", msg.payload.decode())
+    process_message(msg.payload.decode())
 
 # MQTT configuration
 def on_connect(client, userdata, flags, rc):
