a3f6b2ad523d03b79eb78c0bac27c3866117c13a
Trung Thieu Quang <thieuquangtrung1999@gmail.com>
2023-10-29 17:08:19 +0100

    Update awsServer.js

--- Test/FWE_Praktikum/awsServer.js
+++ Test/FWE_Praktikum/awsServer.js
@@ -2,10 +2,18 @@ import express from 'express';
 
 const app = express();
 const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Hello, world');
+
+class Entity {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+app.get('/test', (req, res) => {
+    let e = new Entity(1, 'test');
+    res.send(e);
 })
 
 app.listen(port, () => {
-    console.log(`listening on port${port}`);
+    console.log(`listening on port ${port}`);
 });
